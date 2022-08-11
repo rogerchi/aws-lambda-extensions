500f0b205e5ee81fb4a667fb8c8316748db67d1e
Roger Chi <roger.chi@shipveho.com>
2022-08-11 12:28:51 -0400

    feat: use env variable to define secrets

--- cache-extension-demo/extension/entry.go
+++ cache-extension-demo/extension/entry.go
@@ -5,39 +5,37 @@ package extension
 
 import (
 	"aws-lambda-extensions/cache-extension-demo/plugins"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // Constants definition
 const (
 	Parameters = "parameters"
-	Dynamodb   = "dynamodb"
-	FileName                 = "/var/task/config.yaml"
 	InitializeCacheOnStartup = "CACHE_EXTENSION_INIT_STARTUP"
+	SecretPaths = "CACHE_EXTENSION_SECRET_PATHS"
+	SecretsRegion = "CACHE_EXTENSION_SECRETS_REGION"
 )
 
 // Struct for storing CacheConfiguration
 type CacheConfig struct {
 	Parameters []plugins.ParameterConfiguration
-	Dynamodb   []plugins.DynamodbConfiguration
 }
+
+var secretNames = os.Getenv(SecretPaths)
+var secretsRegion = os.Getenv(SecretsRegion)
 
 var cacheConfig = CacheConfig{}
 
 // Initialize cache and start the background process to refresh cache
 func InitCacheExtensions() {
-	// Read the cache config file
-	data := LoadConfigFile()
-
-	// Unmarshal the configuration to struct
-	err := yaml.Unmarshal([]byte(data), &cacheConfig)
-	if err != nil {
-		log.Fatalf(plugins.PrintPrefix, "error: %v", err)
+	var secretNamesSlice []string = strings.Split(secretNames, ",")
+	var secretNamesWithPathSlice []string
+	for _, secretName := range secretNamesSlice {
+		secretNamesWithPathSlice = append(secretNamesWithPathSlice, "/aws/reference/secretsmanager/" + secretName)
 	}
+	cacheConfig.Parameters = append(cacheConfig.Parameters, plugins.ParameterConfiguration{Region: secretsRegion, Names: secretNamesWithPathSlice})
 
 	// Initialize Cache
 	InitCache()
@@ -62,7 +60,6 @@ func InitCache() {
 
 	// Initialize map and load data from individual services if "CACHE_EXTENSION_INIT_STARTUP" = true
 	plugins.InitParameters(cacheConfig.Parameters, initCacheInBool)
-	plugins.InitDynamodb(cacheConfig.Dynamodb, initCacheInBool)
 }
 
 // Route request to corresponding cache handlers
@@ -70,19 +67,7 @@ func RouteCache(cacheType string, name string) string {
 	switch cacheType {
 	case Parameters:
 		return plugins.GetParameterCache(name)
-	case Dynamodb:
-		return plugins.GetDynamodbCache(name)
 	default:
 		return ""
 	}
-}
-
-// Load the config file
-func LoadConfigFile() string {
-	data, err := ioutil.ReadFile(FileName)
-	if err != nil {
-		panic(err)
-	}
-
-	return string(data)
 }
